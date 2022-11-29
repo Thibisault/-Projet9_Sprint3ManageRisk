af009d83dc3399cb8b46059cb1973c21f056cb95
Thibisault <72867272+Thibisault@users.noreply.github.com>
2022-11-29 07:33:20 +0100

    first commit

---
+++ src/main/java/com/example/Sprint3ManageRisk/Sprint3ManageRiskApplication.java
@@ -0,0 +1,13 @@
+package com.example.Sprint3ManageRisk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sprint3ManageRiskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sprint3ManageRiskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Sprint3ManageRisk/Sprint3ManageRiskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Sprint3ManageRisk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sprint3ManageRiskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
